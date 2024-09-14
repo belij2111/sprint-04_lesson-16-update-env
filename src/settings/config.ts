3e146a7468ac07c290b6c580851a241004ede330
belij2111 <belijvalerij2111@gmail.com>
2024-09-14 22:56:54 +0300

    feat: Add a class to store app settings

---
+++ src/settings/config.ts
@@ -0,0 +1,32 @@
+import { config } from 'dotenv';
+import * as process from 'process';
+
+config();
+
+class AppSettings {
+  constructor(
+    private readonly PORT: string = process.env.PORT ?? '3002',
+    private readonly MONGO_URL: string = process.env.MONGO_URL ?? '',
+    private readonly COLLECTION_NAME = {
+      USER: process.env.USER_COLLECTION_NAME,
+    },
+    private readonly PATH = {
+      USERS: '/users',
+    },
+  ) {}
+
+  getPort(): string {
+    return this.PORT;
+  }
+  getMongoUrl(): string {
+    return this.MONGO_URL;
+  }
+  getCollectionNames() {
+    return this.COLLECTION_NAME;
+  }
+  getPath() {
+    return this.PATH;
+  }
+}
+
+export const appSettings = new AppSettings();
