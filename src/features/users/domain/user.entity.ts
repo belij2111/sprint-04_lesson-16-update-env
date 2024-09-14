76f6c8531688c32fe1f802a4c37350c232d2cebc
belij2111 <belijvalerij2111@gmail.com>
2024-09-15 00:25:42 +0300

    feat: Add a schema for users

---
+++ src/features/users/domain/user.entity.ts
@@ -0,0 +1,20 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+@Schema()
+export class User {
+  @Prop({ type: String, required: true })
+  login: string;
+
+  @Prop({ type: String, required: true })
+  password: string;
+
+  @Prop({ type: String, required: true })
+  email: string;
+
+  @Prop({ type: Date, default: new Date() })
+  createdAt: Date;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
+export type UserDocument = HydratedDocument<User>;
