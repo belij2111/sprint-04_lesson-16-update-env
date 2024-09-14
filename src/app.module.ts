c964484b4692bf005850abc73a7c29665e1ef94b
belij2111 <belijvalerij2111@gmail.com>
2024-09-15 00:29:13 +0300

    feat: Add a database connection and initialize a collection of users

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { appSettings } from './settings/config';
+import { UserSchema } from './features/users/domain/user.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(appSettings.getMongoUrl()),
+    MongooseModule.forFeature([
+      { name: appSettings.getCollectionNames().USERS, schema: UserSchema },
+    ]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
