2441122a7445f35e05ab959e5752ce75e8b5ef54
belij2111 <belijvalerij2111@gmail.com>
2024-09-14 23:01:21 +0300

    feat: Update the bootstrap function to apply the settings

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { applyAppSetting } from './settings/apply-app-setting';
+import { appSettings } from './settings/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3003);
+
+  applyAppSetting(app);
+
+  await app.listen(appSettings.getPort(), () => {
+    console.log('App starting listen port: ', appSettings.getPort());
+  });
 }
 bootstrap();
