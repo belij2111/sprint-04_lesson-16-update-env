688dbbadd86d06bb352b66fac5bf57b63a506f74
belij2111 <belijvalerij2111@gmail.com>
2024-09-18 00:19:32 +0300

    feat: Add field with id user

--- src/features/users/domain/user.entity.ts
+++ src/features/users/domain/user.entity.ts
@@ -1,9 +1,13 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { HydratedDocument, Model } from 'mongoose';
+import { HydratedDocument, Model, ObjectId } from 'mongoose';
 import { appSettings } from '../../../settings/config';
+import * as mongoose from 'mongoose';
 
 @Schema({ collection: appSettings.getCollectionNames().USERS })
 export class User {
+  @Prop({ type: mongoose.Schema.Types.ObjectId, required: true })
+  _id: ObjectId;
+
   @Prop({ type: String, required: true })
   login: string;
 
