77a11fa4f804b627018c2e1dc71a19f6c141446e
belij2111 <belijvalerij2111@gmail.com>
2024-09-22 21:46:52 +0300

    feat: Add methods to get all posts and to get post by ID

---
+++ src/features/content/posts/infrastructure/posts.query-repository.ts
@@ -0,0 +1,64 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import {
+  Paginator,
+  SortQueryFilterType,
+} from '../../../../base/pagination.base.model';
+import { Post, PostDocument, PostModelType } from '../domain/post.entity';
+import { PostOutputModel } from '../api/models/output/post.output.model';
+
+@Injectable()
+export class PostsQueryRepository {
+  constructor(
+    @InjectModel(Post.name) private readonly PostModel: PostModelType,
+  ) {}
+
+  async getAll(
+    inputQuery: SortQueryFilterType,
+  ): Promise<Paginator<PostOutputModel[]>> {
+    const filter = {};
+    const items = await this.PostModel.find(filter)
+      .sort({ [inputQuery.sortBy]: inputQuery.sortDirection })
+      .skip((inputQuery.pageNumber - 1) * inputQuery.pageSize)
+      .limit(inputQuery.pageSize)
+      .lean()
+      .exec();
+    const totalCount = await this.PostModel.countDocuments(filter);
+    return {
+      pagesCount: Math.ceil(totalCount / inputQuery.pageSize),
+      page: inputQuery.pageNumber,
+      pageSize: inputQuery.pageSize,
+      totalCount,
+      items: items.map(this.postMapToOutput),
+    };
+  }
+
+  async getById(id: string): Promise<PostOutputModel | null> {
+    const foundPost = await this.PostModel.findById(id);
+    if (!foundPost) return null;
+    return this.postMapToOutput(foundPost);
+  }
+
+  private postMapToOutput(post: PostDocument): PostOutputModel {
+    const newestLikes = post.extendedLikesInfo.newestLikes.map((el) => ({
+      addedAt: el.addedAt,
+      userId: el.userId,
+      login: el.login,
+    }));
+    return {
+      id: post._id.toString(),
+      title: post.title,
+      shortDescription: post.shortDescription,
+      content: post.content,
+      blogId: post.blogId.toString(),
+      blogName: post.blogName,
+      createdAt: post.createdAt,
+      extendedLikesInfo: {
+        likesCount: post.extendedLikesInfo.likesCount,
+        dislikesCount: post.extendedLikesInfo.dislikesCount,
+        myStatus: post.extendedLikesInfo.myStatus,
+        newestLikes: newestLikes,
+      },
+    };
+  }
+}
