b6a257176eb4234c08a5bca7f7e8a1285ba7860b
belij2111 <belijvalerij2111@gmail.com>
2024-09-25 22:59:42 +0300

    feat: Add the function for global setup of pipes

--- src/settings/apply-app-setting.ts
+++ src/settings/apply-app-setting.ts
@@ -1,11 +1,16 @@
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 
 const APP_PREFIX = '/';
 
 export const applyAppSetting = (app: INestApplication) => {
   setAppPrefix(app);
+  setAppPipes(app);
 };
 
 const setAppPrefix = (app: INestApplication) => {
   app.setGlobalPrefix(APP_PREFIX);
+};
+
+const setAppPipes = (app: INestApplication) => {
+  app.useGlobalPipes(new ValidationPipe());
 };
