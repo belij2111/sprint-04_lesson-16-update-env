8e748da24757d520abd9a62b618d8f6c2338bf46
belij2111 <belijvalerij2111@gmail.com>
2024-09-26 16:03:02 +0300

    feat: Update the format of outgoing error messages

--- src/common/exeption-filters/http-exeption-filter.ts
+++ src/common/exeption-filters/http-exeption-filter.ts
@@ -6,6 +6,10 @@ import {
 } from '@nestjs/common';
 import { Request, Response } from 'express';
 
+interface ErrorResponse {
+  message: string | string[];
+}
+
 @Catch(HttpException)
 export class HttpExceptionFilter implements ExceptionFilter {
   catch(exception: HttpException, host: ArgumentsHost) {
@@ -14,10 +18,29 @@ export class HttpExceptionFilter implements ExceptionFilter {
     const request = ctx.getRequest<Request>();
     const status = exception.getStatus();
 
-    response.status(status).json({
-      statusCode: status,
-      timestamp: new Date().toISOString(),
-      path: request.url,
-    });
+    if (status === 400) {
+      const errorsResponse: { errorsMessages: string[] } = {
+        errorsMessages: [],
+      };
+      const responseBody: ErrorResponse =
+        exception.getResponse() as ErrorResponse;
+      if (responseBody.message) {
+        if (Array.isArray(responseBody.message)) {
+          responseBody.message.forEach((e) =>
+            errorsResponse.errorsMessages.push(e),
+          );
+        } else {
+          errorsResponse.errorsMessages.push(responseBody.message);
+        }
+      }
+
+      response.status(status).json(errorsResponse);
+    } else {
+      response.status(status).json({
+        statusCode: status,
+        timestamp: new Date().toISOString(),
+        path: request.url,
+      });
+    }
   }
 }
--- src/settings/apply-app-setting.ts
+++ src/settings/apply-app-setting.ts
@@ -1,4 +1,8 @@
-import { INestApplication, ValidationPipe } from '@nestjs/common';
+import {
+  BadRequestException,
+  INestApplication,
+  ValidationPipe,
+} from '@nestjs/common';
 import { HttpExceptionFilter } from '../common/exeption-filters/http-exeption-filter';
 
 const APP_PREFIX = '/';
@@ -14,7 +18,27 @@ const setAppPrefix = (app: INestApplication) => {
 };
 
 const setAppPipes = (app: INestApplication) => {
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      stopAtFirstError: true,
+      exceptionFactory: (errors) => {
+        const customErrors: { message: string; field: string }[] = [];
+        errors.forEach((err) => {
+          const constrainKeys = Object.keys(err.constraints || {});
+          constrainKeys.forEach((cKey, index) => {
+            if (index >= 1) return;
+            const msg = err.constraints?.[cKey] as any;
+            customErrors.push({
+              message: msg,
+              field: err.property,
+            });
+          });
+        });
+        throw new BadRequestException(customErrors);
+      },
+    }),
+  );
 };
 
 const setAppExceptionsFilters = (app: INestApplication) => {
