0140a32f94953df2e6ca66b8b079eff2eb6e0e83
belij2111 <belijvalerij2111@gmail.com>
2024-09-27 21:49:53 +0300

    refactor: Update content module

--- src/features/content/content.module.ts
+++ src/features/content/content.module.ts
@@ -1,8 +1,29 @@
 import { Module } from '@nestjs/common';
-import { BlogsModule } from './blogs/blogs.module';
-import { PostsModule } from './posts/posts.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Blog, BlogSchema } from './blogs/domain/blog.entity';
+import { Post, PostSchema } from './posts/domain/post.entity';
+import { PostsController } from './posts/api/posts.controller';
+import { BlogsService } from './blogs/application/blogs.service';
+import { BlogsRepository } from './blogs/infrastructure/blogs.repository';
+import { BlogsQueryRepository } from './blogs/infrastructure/blogs.query-repository';
+import { PostsService } from './posts/application/posts.service';
+import { PostsRepository } from './posts/infrastructure/posts.repository';
+import { PostsQueryRepository } from './posts/infrastructure/posts.query-repository';
+import { BlogsController } from './blogs/api/blogs.controller';
 
 @Module({
-  imports: [BlogsModule, PostsModule],
+  imports: [
+    MongooseModule.forFeature([{ name: Blog.name, schema: BlogSchema }]),
+    MongooseModule.forFeature([{ name: Post.name, schema: PostSchema }]),
+  ],
+  controllers: [BlogsController, PostsController],
+  providers: [
+    BlogsService,
+    BlogsRepository,
+    BlogsQueryRepository,
+    PostsService,
+    PostsRepository,
+    PostsQueryRepository,
+  ],
 })
 export class ContentModule {}
