403235933405b38ced669725c66475fbb4b9c498
belij2111 <belijvalerij2111@gmail.com>
2024-09-27 23:40:18 +0300

    feat: Add validation of input data for posts

--- src/features/content/posts/api/models/input/create-post.input.model.ts
+++ src/features/content/posts/api/models/input/create-post.input.model.ts
@@ -1,6 +1,25 @@
+import { TrimIsString } from '../../../../../../common/decorators/validate/trim-is-string';
+import { Length } from 'class-validator';
+
 export class PostCreateModel {
+  @TrimIsString()
+  @Length(3, 30, {
+    message: 'title length should be from 3 to 30 symbol',
+  })
   title: string;
+
+  @TrimIsString()
+  @Length(3, 100, {
+    message: 'shortDescription length should be from 3 to 100 symbol',
+  })
   shortDescription: string;
+
+  @TrimIsString()
+  @Length(3, 1000, {
+    message: 'content length should be from 3 to 1000 symbol',
+  })
   content: string;
+
+  @TrimIsString()
   blogId: string;
 }
