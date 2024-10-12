a1c1621a38056e58bdcb0a2c42d61f8bb90ebee3
belij2111 <belijvalerij2111@gmail.com>
2024-10-12 17:14:44 +0300

    feat: Add authentication settings

--- src/settings/env/api-settings.ts
+++ src/settings/env/api-settings.ts
@@ -1,10 +1,14 @@
 import { EnvironmentVariable } from './configuration';
-import { IsNumber } from 'class-validator';
+import { IsNumber, IsString } from 'class-validator';
 
 export class ApiSettings {
   constructor(private environmentVariables: EnvironmentVariable) {}
   @IsNumber()
   PORT: number = Number(this.environmentVariables.PORT);
+  @IsString()
+  ADMIN_LOGIN: string = this.environmentVariables.ADMIN_LOGIN;
+  @IsString()
+  ADMIN_PASSWORD: string = this.environmentVariables.ADMIN_PASSWORD;
   PATH = {
     USERS: this.environmentVariables.USERS_PATH,
     BLOGS: this.environmentVariables.BLOGS_PATH,
