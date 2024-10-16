32be014482b200eaa221cfb20cd1cda1c8825c6e
belij2111 <belijvalerij2111@gmail.com>
2024-10-16 21:57:11 +0300

    refactor: Update getting data for a JwtModule from environment variables

--- src/features/auth/auth.module.ts
+++ src/features/auth/auth.module.ts
@@ -7,14 +7,24 @@ import { AuthService } from './application/auth.service';
 import { JwtModule } from '@nestjs/jwt';
 import { LocalStrategy } from '../../common/strategies/local.strategy';
 import { BcryptService } from '../../base/bcrypt.service';
+import { ConfigService } from '@nestjs/config';
+import { ConfigurationType } from '../../settings/env/configuration';
 
 @Module({
   imports: [
     PassportModule,
     UsersModule,
-    JwtModule.register({
-      secret: '123456789',
-      signOptions: { expiresIn: '100m' },
+    JwtModule.registerAsync({
+      useFactory: (configService: ConfigService<ConfigurationType, true>) => {
+        const apiSettings = configService.get('apiSettings', {
+          infer: true,
+        });
+        return {
+          secret: apiSettings.JWT_SECRET,
+          signOptions: { expiresIn: apiSettings.ACCESS_TOKEN_EXPIRATION },
+        };
+      },
+      inject: [ConfigService],
     }),
   ],
   controllers: [AuthController],
