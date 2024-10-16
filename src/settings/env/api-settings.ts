bb5738f16c09639f5f896ce9b48327df41e4f1eb
belij2111 <belijvalerij2111@gmail.com>
2024-10-16 20:13:34 +0300

    refactor: Add JWT_SECRET and JWT_EXPIRATION for new environment variables in ApiSettings

--- src/settings/env/api-settings.ts
+++ src/settings/env/api-settings.ts
@@ -9,6 +9,10 @@ export class ApiSettings {
   ADMIN_LOGIN: string = this.environmentVariables.ADMIN_LOGIN;
   @IsString()
   ADMIN_PASSWORD: string = this.environmentVariables.ADMIN_PASSWORD;
+  @IsString()
+  JWT_SECRET: string = this.environmentVariables.JWT_SECRET;
+  @IsString()
+  JWT_EXPIRATION: string = this.environmentVariables.JWT_EXPIRATION;
   PATH = {
     USERS: this.environmentVariables.USERS_PATH,
     BLOGS: this.environmentVariables.BLOGS_PATH,
