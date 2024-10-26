2d8ea20e7e5862c9e8e38b49350ff4df886225eb
belij2111 <belijvalerij2111@gmail.com>
2024-10-26 17:01:20 +0300

    feat: Update the schema for the user

--- src/features/users/domain/user.entity.ts
+++ src/features/users/domain/user.entity.ts
@@ -2,6 +2,20 @@ import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { HydratedDocument, Model } from 'mongoose';
 
 @Schema()
+class EmailConfirmation {
+  @Prop({ type: String, required: true })
+  confirmationCode: string;
+
+  @Prop({ type: Date, required: true })
+  expirationDate: Date;
+
+  @Prop({ type: Boolean, required: true, default: false })
+  isConfirmed: boolean;
+}
+
+const EmailConfirmationSchema = SchemaFactory.createForClass(EmailConfirmation);
+
+@Schema()
 export class User {
   @Prop({ type: String, required: true })
   login: string;
@@ -14,6 +28,9 @@ export class User {
 
   @Prop({ type: Date, default: new Date() })
   createdAt: Date;
+
+  @Prop({ type: EmailConfirmationSchema, required: true })
+  emailConfirmation: EmailConfirmation;
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
