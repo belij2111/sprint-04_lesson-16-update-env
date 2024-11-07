0c677b0f00e533e47542f767062a1b1de15e4b7a
belij2111 <belijvalerij2111@gmail.com>
2024-11-07 17:10:50 +0300

    feat: Add a schema for comments

---
+++ src/features/bloggers-platform/comments/domain/comment.entity.ts
@@ -0,0 +1,46 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument, Model } from 'mongoose';
+
+@Schema()
+class CommentatorInfo {
+  @Prop({ type: String, required: true })
+  userId: string;
+  @Prop({ type: String, required: true })
+  userLogin: string;
+}
+
+const CommentatorInfoSchema = SchemaFactory.createForClass(CommentatorInfo);
+
+@Schema()
+class LikesInfo {
+  @Prop({ type: Number, default: 0 })
+  likesCount: number;
+  @Prop({ type: Number, default: 0 })
+  dislikesCount: number;
+  @Prop({ type: String, enum: ['None', 'Like', 'Dislike'], default: 'None' })
+  myStatus: string;
+}
+
+const LikesInfoSchema = SchemaFactory.createForClass(LikesInfo);
+
+@Schema()
+export class Comment {
+  @Prop({ type: String, required: true })
+  content: string;
+
+  @Prop({ type: CommentatorInfoSchema, required: true })
+  commentatorInfo: CommentatorInfo;
+
+  @Prop({ type: Date, required: true, default: new Date() })
+  createdAt: Date;
+
+  @Prop({ type: String, required: true })
+  postId: string;
+
+  @Prop({ type: LikesInfoSchema, required: true })
+  likesInfo: LikesInfo;
+}
+
+export const CommentSchema = SchemaFactory.createForClass(Comment);
+export type CommentDocument = HydratedDocument<Comment>;
+export type CommentModelType = Model<CommentDocument>;
