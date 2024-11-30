227625117c8b55c9315c967eec28b2724b77bc1f
belij2111 <belijvalerij2111@gmail.com>
2024-11-30 19:28:05 +0300

    feat: Add a schema for security-devices

---
+++ src/features/security-devices/domain/security-devices.entity.ts
@@ -0,0 +1,28 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument, Model } from 'mongoose';
+
+@Schema({ _id: false })
+export class SecurityDevices {
+  @Prop({ type: String, required: true })
+  userId: string;
+
+  @Prop({ type: String, required: true })
+  deviceId: string;
+
+  @Prop({ type: String, required: true })
+  ip: string;
+
+  @Prop({ type: String, required: true })
+  deviceName: string;
+
+  @Prop({ type: Date, required: true })
+  iatDate: Date;
+
+  @Prop({ type: Date, required: true })
+  expDate: Date;
+}
+
+export const SecurityDevicesSchema =
+  SchemaFactory.createForClass(SecurityDevices);
+export type SecurityDevicesDocument = HydratedDocument<SecurityDevices>;
+export type SecurityDevicesModelType = Model<SecurityDevices>;
