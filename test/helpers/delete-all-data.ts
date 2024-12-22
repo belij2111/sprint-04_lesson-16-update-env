49c06290ff8d94788bb5bdd9d7c8e17c036e0cf9
belij2111 <belijvalerij2111@gmail.com>
2024-12-23 00:34:25 +0300

    fix(test): Update the path

--- test/helpers/delete-all-data.ts
+++ test/helpers/delete-all-data.ts
@@ -2,5 +2,5 @@ import { INestApplication } from '@nestjs/common';
 import request from 'supertest';
 
 export const deleteAllData = async (app: INestApplication) => {
-  return request(app.getHttpServer()).delete(`/api/testing/all-data`);
+  return request(app.getHttpServer()).delete(`/testing/all-data`);
 };
