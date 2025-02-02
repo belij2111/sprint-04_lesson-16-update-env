cee5e745805dac83343ca7a26d17564dfcb78fbd
belij2111 <belijvalerij2111@gmail.com>
2025-02-02 13:30:19 +0300

    feat(test): Add a model for several users

--- test/models/user-accounts/user.input.model.ts
+++ test/models/user-accounts/user.input.model.ts
@@ -4,7 +4,7 @@ export const createValidUserModel = (count: number = 1): UserCreateModel => {
   const userModel = new UserCreateModel();
   userModel.login = `User_${count}`;
   userModel.password = `qwerty_${count}`;
-  userModel.email = `user_${count}@gmail.com`;
+  userModel.email = `user_${count}@gmail.com`; // `belijtestback7777@gmail.com`; //`user_${count}@gmail.com`;
   return userModel;
 };
 
@@ -14,4 +14,18 @@ export const createInValidUserModel = (count: number = 1): UserCreateModel => {
   invalidUserModel.password = `qwerty_${count}`;
   invalidUserModel.email = `invalid email${count}`;
   return invalidUserModel;
+};
+
+export const createSeveralUsersModels = (
+  count: number = 1,
+): UserCreateModel[] => {
+  const users: UserCreateModel[] = [];
+  for (let i = 1; i <= count; i++) {
+    const userModel = new UserCreateModel();
+    userModel.login = `User_${i}`;
+    userModel.password = `qwerty_${i}`;
+    userModel.email = `user_${i}@gmail.com`;
+    users.push(userModel);
+  }
+  return users;
 };
